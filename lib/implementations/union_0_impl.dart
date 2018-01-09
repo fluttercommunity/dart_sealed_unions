f631807efa8392389a079ae84faa049938a9277d
Brian Egan <brian@brianegan.com>
2018-01-09 11:44:42 +0100

    Consistent Naming: Union0View -> Union0Impl

--- lib/implementations/union_0_impl.dart
+++ lib/implementations/union_0_impl.dart
@@ -1,10 +1,10 @@
 import 'package:func/func.dart';
 import 'package:sealed_unions/union_0.dart';
 
-class Union0View<A> implements Union0<A> {
+class Union0Impl<A> implements Union0<A> {
   final Union0<A> _union;
 
-  Union0View(Union0<A> union) : _union = union;
+  Union0Impl(Union0<A> union) : _union = union;
 
   @override
   void continued(VoidFunc1<A> continuationFirst) {
@@ -19,7 +19,7 @@ class Union0View<A> implements Union0<A> {
   @override
   bool operator ==(Object other) =>
       identical(this, other) ||
-          other is Union0View &&
+          other is Union0Impl &&
               runtimeType == other.runtimeType &&
               _union == other._union;
 
